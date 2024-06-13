ba73f94033e624a5ad2f95066a3a08e19fe53538
matheusvirissimo <matheus.virissimo@unesp.br>
2024-06-13 18:32:55 -0300

    Calcular salário, getters e setters de Técnico

--- Tecnico/Tecnico.java
+++ Tecnico/Tecnico.java
@@ -4,9 +4,34 @@ package Tecnico;
 
 public class Tecnico extends funcionario.Funcionario{
     
+    private String funcao;
+    
+    public Tecnico(String codigo, String nome, double salario, String nivel, String funcao){
+        super(codigo, nome, salario, nivel);
+        setFuncao(funcao);
+    }
+    
     @Override 
     public double calcularSalario(){
+        if(nivel.equalsIgnoreCase("t1")){
+            salario = salario + (salario *0.1);
+        }else{
+            if(nivel.equalsIgnoreCase("t2")){
+                salario = salario + (salario * 0.2);
+            }else{
+                System.out.println("Nível não existente.");
+            }
+        }
         
+        return salario;
         
     } 
+
+    public String getFuncao() {
+        return funcao;
+    }
+    
+    public void setFuncao(String funcao){
+        this.salario = salario;
+    }
 }
