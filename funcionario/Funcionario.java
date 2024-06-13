15c0f7a1cc2939c20cb6c24d08e14c0ef4f44110
matheusvirissimo <matheus.virissimo@unesp.br>
2024-06-13 18:14:53 -0300

    Getters e setters de Funcionário

--- funcionario/Funcionario.java
+++ funcionario/Funcionario.java
@@ -9,4 +9,36 @@ public abstract class Funcionario {
     
     public abstract double calcularSalario(); // todas as classes que se extendem precisam
     
+    public String getCodigo() {
+        return codigo;
+    }
+    
+    public void setCodigo(String codigo){
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    } 
+
+    public double getSalario() {
+        return salario;
+    }
+    
+    public void setSalario(double salario){
+        this.salario = salario;
+    }
+
+    public String getNivel() {
+        return nivel;
+    }
+    
+    public void setNivel(String nivel){
+        this.nivel = nivel;
+    }
+    
 }
