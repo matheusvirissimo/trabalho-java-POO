8b0b31abd9aa338c3ffda1c152e1b5f5d69c44dd
matheusvirissimo <matheus.virissimo@unesp.br>
2024-06-23 18:18:15 -0300

    Adição de constantes para cálculo do salário dos funcionários

---
+++ base/Utils.java
@@ -0,0 +1,17 @@
+package base;
+
+public class Utils {
+    // Técnicos
+    public static final double T1 = 0.1;
+    public static final double T2 = 0.2;
+    
+    // Efetivos
+    public static final double D1 = 0.05;
+    public static final double D2 = 0.1;
+    public static final double D3 = 0.2;
+    
+    // Substitutos
+    public static final double S1 = 0.05;
+    public static final double S2 = 0.1;
+    
+}
